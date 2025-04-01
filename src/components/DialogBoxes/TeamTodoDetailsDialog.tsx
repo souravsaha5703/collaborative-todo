d7cce59dd135b6049082d86f902251adb7d7b1df
Sourav Saha <saha83787@gmail.com>
2025-04-01 19:31:09 +0530

    team todos ui problem fixed

--- src/components/DialogBoxes/TeamTodoDetailsDialog.tsx
+++ src/components/DialogBoxes/TeamTodoDetailsDialog.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
-import { Calendar, Clock, Paperclip, Send, Tag, User } from "lucide-react";
+import { Calendar, User, UserPen } from "lucide-react";
 import { TeamTodosInterface } from '@/utils/AppInterfaces';
 import { useAppSelector } from '@/hooks/redux-hooks';
 
@@ -20,6 +20,15 @@ const TeamTodoDetailsDialog: React.FC<DialogProps> = ({ teamTodo, isDialogOpen, 
             userAvatar = member.user_avatar;
         }
     });
+    let creatorAvatar: string | undefined = '';
+    let creatorName: string | undefined = '';
+    teamData?.members.forEach(member => {
+        if (member.id == teamTodo?.createdBy) {
+            creatorAvatar = member.user_avatar;
+            creatorName = member.user_name;
+        }
+    });
+
     return (
         <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
             <DialogContent className={`sm:max-w-[700px]`}>
@@ -50,18 +59,18 @@ const TeamTodoDetailsDialog: React.FC<DialogProps> = ({ teamTodo, isDialogOpen, 
                     </span>
                 </div>
                 <div className='flex flex-col'>
-                    <h3 className="text-3xl font-noto text-start font-medium">{teamTodo?.task}</h3>
+                    <h3 className="text-3xl mb-2 font-noto text-start font-medium">{teamTodo?.task}</h3>
                     {teamTodo?.task_status == true ? (
                         <Badge
                             variant="outline"
-                            className="bg-green-400 font-noto text-xs font-normal text-green-700 border-green-400"
+                            className="bg-green-400 w-16 font-noto text-xs font-normal text-green-700 border-green-400"
                         >
                             Completed
                         </Badge>
                     ) : (
                         <Badge
                             variant="outline"
-                            className="bg-red-300 font-noto text-xs font-normal text-red-700 border-red-300"
+                            className="bg-yellow-300 w-16 font-noto text-xs font-normal text-yellow-700 border-yellow-300"
                         >
                             Pending
                         </Badge>
@@ -69,8 +78,26 @@ const TeamTodoDetailsDialog: React.FC<DialogProps> = ({ teamTodo, isDialogOpen, 
                 </div>
                 <div className="space-y-3 rounded-lg border p-3">
                     <div className="flex items-center gap-2 text-sm">
+                        <UserPen className="h-4 w-4 text-muted-foreground" />
+                        <span className="text-muted-foreground">Created by :</span>
+                        <div className='flex items-center gap-2'>
+                            <Avatar
+                                className="size-10 cursor-pointer hover:ring-2 hover:ring-primary/50"
+                            >
+                                <AvatarImage src={creatorAvatar} alt={creatorName} />
+                                <AvatarFallback>
+                                    {creatorName
+                                        .split(" ")
+                                        .map((n) => n[0])
+                                        .join("")}
+                                </AvatarFallback>
+                            </Avatar>
+                            <p className="text-lg font-normal font-noto">{creatorName}</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-2 text-sm">
                         <User className="h-4 w-4 text-muted-foreground" />
-                        <span className="text-muted-foreground">Assigned to:</span>
+                        <span className="text-muted-foreground">Assigned to :</span>
                         {teamTodo?.assigned_to.user_name == "none" ? (
                             <></>
                         ) : (
@@ -92,12 +119,12 @@ const TeamTodoDetailsDialog: React.FC<DialogProps> = ({ teamTodo, isDialogOpen, 
                     </div>
                     <div className="flex items-center gap-2 text-sm">
                         <Calendar className="h-4 w-4 text-muted-foreground" />
-                        <span className="text-muted-foreground">Due date: </span>
+                        <span className="text-muted-foreground">Due date : </span>
                         <p className="text-lg font-normal font-noto mb-1">{teamTodo?.task_due_date}</p>
                     </div>
                     <div className="flex items-center gap-2 text-sm">
                         <Calendar className="h-4 w-4 text-muted-foreground" />
-                        <span className="text-muted-foreground">Task Completed date: </span>
+                        <span className="text-muted-foreground">Task Completed date : </span>
                         <p className="text-lg font-normal font-noto mb-1">
                             {teamTodo?.task_completed_date == '' ? (
                                 "Not Yet Completed"
@@ -106,29 +133,16 @@ const TeamTodoDetailsDialog: React.FC<DialogProps> = ({ teamTodo, isDialogOpen, 
                             )}
                         </p>
                     </div>
+                    {teamTodo?.task_updated_at == '' ? (
+                        <></>
+                    ) : (
+                        <div className="flex items-center gap-2 text-sm">
+                            <Calendar className="h-4 w-4 text-muted-foreground" />
+                            <span className="text-muted-foreground">Task Updated At: </span>
+                            <p className="text-lg font-normal font-noto mb-1">{teamTodo?.task_updated_at}</p>
+                        </div>
+                    )}
                 </div>
-
-
-                {/* <div>
-                            <h3 className="mb-2 text-sm font-medium">Actions</h3>
-                            <div className="space-y-2">
-                                <Button variant="outline" size="sm" className="w-full justify-start" onClick={onAssign}>
-                                    <User className="mr-2 h-4 w-4" />
-                                    {task.assignedTo ? "Change Assignee" : "Assign Task"}
-                                </Button>
-                                <Button variant="outline" size="sm" className="w-full justify-start">
-                                    <Calendar className="mr-2 h-4 w-4" />
-                                    Change Due Date
-                                </Button>
-                                <Button variant="outline" size="sm" className="w-full justify-start">
-                                    <Paperclip className="mr-2 h-4 w-4" />
-                                    Add Attachment
-                                </Button>
-                                <Button variant="outline" size="sm" className="w-full justify-start text-destructive">
-                                    Delete Task
-                                </Button>
-                            </div>
-                        </div> */}
             </DialogContent>
         </Dialog>
     )
