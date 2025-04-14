04d253fdebeb3cddab8e3276a122cecc3bf70111
Sourav Saha <saha83787@gmail.com>
2025-04-14 20:12:50 +0530

    responsiveness added for team members page

--- src/Pages/Main/Teams/TeamMembers.tsx
+++ src/Pages/Main/Teams/TeamMembers.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '@/components/NavigationBars/Sidebar';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Copy } from "lucide-react";
+import { Copy, Check } from "lucide-react";
 import { useAppSelector } from '@/hooks/redux-hooks';
+import { toast } from '@/hooks/use-toast';
 
 const TeamMembers: React.FC = () => {
+    const [isCopied, setIsCopied] = useState(false);
     const teamData = useAppSelector((state) => state.team.currentTeam);
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(teamData?.invite_code ?? "")
+            .then(() => {
+                setIsCopied(true);
+                toast({
+                    title: "Copied!",
+                    description: "Invite code copied to clipboard",
+                    duration: 2000,
+                });
+
+                setTimeout(() => {
+                    setIsCopied(false);
+                }, 2000);
+            })
+            .catch(err => {
+                console.error('Failed to copy: ', err);
+                toast({
+                    title: "Error",
+                    description: "Failed to copy invite code",
+                    variant: "destructive",
+                    duration: 2000,
+                });
+            });
+    };
 
     return (
         <>
             <Sidebar />
             <div className='md:ml-20 p-5 bg-background'>
-                <div className='w-full p-2 flex flex-col gap-2'>
-                    <h1 className="text-3xl font-noto font-bold tracking-tight">{teamData?.team_name}</h1>
-                    <p className="text-muted-foreground font-noto text-lg">{teamData?.team_description}</p>
+                <div className='w-full p-2 flex flex-col gap-2 mb-10'>
+                    <h1 className="text-3xl font-noto font-bold tracking-tight max-[487px]:text-xl">{teamData?.team_name}</h1>
+                    <p className="text-muted-foreground font-noto text-lg max-[487px]:text-base">{teamData?.team_description}</p>
                     <Card className="mb-8 mt-4">
                         <CardHeader>
-                            <CardTitle className='font-noto text-xl'>Invite Members</CardTitle>
-                            <CardDescription className='font-noto text-base'>
+                            <CardTitle className='font-noto text-xl max-[487px]:text-base'>Invite Members</CardTitle>
+                            <CardDescription className='font-noto text-base max-[487px]:text-sm'>
                                 Add new members to your team by sharing the invite code or sending an email invitation.
                             </CardDescription>
                         </CardHeader>
@@ -29,9 +56,13 @@ const TeamMembers: React.FC = () => {
                                 <div className="flex-1 space-y-1">
                                     <p className="text-base font-noto font-medium">Team Invite Code</p>
                                     <div className="flex items-center gap-2">
-                                        <code className="rounded font-noto bg-muted px-4 py-2 font-normal text-base">{teamData?.invite_code}</code>
-                                        <Button variant="ghost" size="icon" className="h-8 w-8">
-                                            <Copy className="h-4 w-4" />
+                                        <code className="rounded font-noto bg-muted px-4 py-2 font-normal text-base max-[487px]:text-sm">{teamData?.invite_code}</code>
+                                        <Button onClick={copyToClipboard} aria-label="Copy invite code" variant="ghost" size="icon" className="h-8 w-8">
+                                            {isCopied ? (
+                                                <Check className="h-4 w-4 text-green-500" />
+                                            ) : (
+                                                <Copy className="h-4 w-4" />
+                                            )}
                                             <span className="sr-only">Copy invite code</span>
                                         </Button>
                                     </div>
@@ -42,8 +73,8 @@ const TeamMembers: React.FC = () => {
                     </Card>
                     <Card>
                         <CardHeader>
-                            <CardTitle className='font-noto text-xl'>Team Members</CardTitle>
-                            <CardDescription className='font-noto text-base'>Your team has {teamData?.members.length} members.</CardDescription>
+                            <CardTitle className='font-noto text-xl max-[487px]:text-base'>Team Members</CardTitle>
+                            <CardDescription className='font-noto text-base max-[487px]:text-sm'>Your team has {teamData?.members.length} members.</CardDescription>
                         </CardHeader>
                         <CardContent>
                             <div className='space-y-4'>
@@ -52,8 +83,8 @@ const TeamMembers: React.FC = () => {
                             <div className="space-y-4">
                                 {teamData?.members.map((member) => (
                                     <div key={member.id} className="flex items-center justify-between gap-4">
-                                        <div className="flex items-center gap-4">
-                                            <Avatar>
+                                        <div className="flex items-center gap-4 max-[425px]:gap-2">
+                                            <Avatar className='max-[425px]:size-6'>
                                                 <AvatarImage src={member.user_avatar} alt={member.user_name} />
                                                 <AvatarFallback>
                                                     {member.user_name
@@ -63,8 +94,8 @@ const TeamMembers: React.FC = () => {
                                                 </AvatarFallback>
                                             </Avatar>
                                             <div>
-                                                <p className="font-medium font-noto">{member.user_name}</p>
-                                                <p className="text-sm text-muted-foreground font-noto">{member.user_email}</p>
+                                                <p className="font-medium font-noto max-[487px]:text-sm max-[375px]:w-28 max-[375px]:truncate">{member.user_name}</p>
+                                                <p className="text-sm text-muted-foreground font-noto max-[375px]:w-28 max-[375px]:truncate">{member.user_email}</p>
                                             </div>
                                         </div>
                                         <div className="flex items-center gap-2">
