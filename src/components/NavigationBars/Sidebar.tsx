21243e7490131e730dec6eedae136c7371700aa5
Sourav Saha <saha83787@gmail.com>
2025-05-01 13:56:00 +0530

    sidebar updated

--- src/components/NavigationBars/Sidebar.tsx
+++ src/components/NavigationBars/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { PanelsTopLeft } from 'lucide-react';
 import { ClipboardList } from 'lucide-react';
@@ -7,6 +7,7 @@ import { HeartHandshake } from 'lucide-react';
 import { NavLink } from 'react-router-dom';
 import { ModeToggle } from '../Theme/Mode-toggle';
 import { useAppSelector } from '../../hooks/redux-hooks';
+import { database, storage } from '@/Appwrite/appwriteConfig';
 import {
     Tooltip,
     TooltipContent,
@@ -15,14 +16,38 @@ import {
 } from "@/components/ui/tooltip";
 
 const Sidebar: React.FC = () => {
+    const [profileImage, setProfileImage] = useState<string>('');
     const user = useAppSelector((state) => state.user.currentUser);
+
+    useEffect(() => {
+        const fetchProfileImage = async () => {
+            try {
+                if (user?.id) {
+                    const userData = await database.getDocument(
+                        import.meta.env.VITE_APPWRITE_TODO_DB_ID,
+                        import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+                        user.id
+                    );
+
+                    if (userData.profileImage) {
+                        const imageUrl = storage.getFileView(import.meta.env.VITE_APPWRITE_PROFILE_IMAGE_BUCKET_ID, userData.profileImage);
+                        setProfileImage(imageUrl);
+                    }
+                }
+            } catch (error) {
+                console.error('Error fetching user profile:', error);
+            }
+        }
+
+        fetchProfileImage();
+    }, []);
     return (
         <nav className='p-2 fixed z-50 left-0 bottom-0 w-full bg-background drop-shadow-2xl overflow-hidden max-md:rounded-t-2xl md:rounded-r-2xl md:top-0 md:bottom-auto md:w-20 md:h-screen'>
             <div className='w-full h-full rounded-r-xl flex gap-4 items-center md:flex-col md:py-4 max-md:justify-center max-md:flex-row-reverse'>
                 <NavLink to={'/user/profile'}>
                     <Avatar className='max-md:size-7'>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>CN</AvatarFallback>
+                        <AvatarImage src={profileImage} />
+                        <AvatarFallback>{user?.fullname.charAt(0).toUpperCase()}</AvatarFallback>
                     </Avatar>
                 </NavLink>
                 <p className='text-sm text-center font-noto font-normal max-md:hidden'>{user?.fullname}</p>
