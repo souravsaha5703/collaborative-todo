6b426fe35b52c7011fcbb58df7f5c2d23a7ed120
Sourav Saha <saha83787@gmail.com>
2025-05-02 13:56:19 +0530

    pwa functionality added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA({ registerType: 'autoUpdate',devOptions:{enabled: true},workbox:{globPatterns: ['**/*.{js,css,html,ico,png,svg}']},manifest: false  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
